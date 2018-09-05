e7e601784d93a396850a25e0ff84af33b8f8cf73
Anand V V <anand.vijay@gmail.com>
2018-09-04 23:57:18 -0700

    Initial Commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+
+//route handler
+
+app.get('/', (req, res) => {
+    res.send({hi: 'there'});
+});
+
+//get the underlying port number from HEROKU 
+//or any other environment
+const PORT = process.env.PORT || 5000
+app.listen(PORT);
+
+
